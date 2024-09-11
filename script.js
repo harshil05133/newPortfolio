26024b7915e4eda88e597f16f8dc1ab6fb6a87c9
harshil05133 <rathodharshil05133@gmail.com>
2024-09-11 19:15:13 -0400

    intial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function (){
+  const body = document.body;
+  const themeToggle = document.getElementById('theme-toggle');
+
+  function toggleTheme(){
+    body.classList.toggle('dark-theme');
+
+    if (body.classList.contains('dark-theme')){
+      themeToggle.textContent('Light');
+    } 
+    else{
+      themeToggle.textContent('Dark');
+    }
+  }
+
+  themeToggle.addEventListener('click', toggleTheme);
+});
